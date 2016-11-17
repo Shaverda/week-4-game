343052db4a2bfb8a61ad1f622c9564536b324194
Shaverda <shaverda@gmail.com>
2016-11-16 18:20:58 -0600

    finished as much as i will

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,4 +1,4 @@
-var characters = {
+var characters = {		//object of objects to hold various characters
 	"Rogue" : {
 		health_points: 150,
 		attack_power: 15,
@@ -20,18 +20,13 @@ var characters = {
 		base_attack_power: 13,
 		counter_attack: 13}
 };
-//game currently lets u choose a new enemy even if ur attacker is dead
-//also lets you de-select character chosen by click on it after enemy chosen
+//lets you de-select character chosen by click on it after enemy has been chosen fix it shelby
 
-var is_enemy_chosen = false;	//DOES THIS EVEN WORK BRO			//var to make you only allowed to choose one enemy
-var is_character_chosen = false;	//DOES THIS EVEN WORK BRO				//var to make you only allowed to choose one character
 
-function choose_enemy (is_enemy_chosen){	
+function choose_enemy (){	
 	$(".character-boxes").on("click", function(){
-		if (is_enemy_chosen == false){	
-			enemy_chosen = this;						//sets var enemy_chosen to item clicked on
+		if ($(".enemy-chosen").children().length == 1){	//makes sure an enemy isn't already chosen
 			$(".enemy-chosen").append(this);			//moves enemy clicked on to defender area
-			is_enemy_chosen = true;						//ensures you can only choose 1 enemy
 			$(".alert-text").html("Now...fight!");
 		}					
 	})
@@ -39,8 +34,7 @@ function choose_enemy (is_enemy_chosen){
 
 $(document).ready(function() {						
 	var character_chosen;								//stores your chosen character
-	var enemy_chosen;									//stores your chosen enemy
-	document.querySelector(".rogue_health_points").innerHTML = (characters.Rogue.health_points);
+	document.querySelector(".rogue_health_points").innerHTML = (characters.Rogue.health_points);	//writes number of hP per char
 	document.querySelector(".dk_health_points").innerHTML = (characters.DeathKnight.health_points);
 	document.querySelector(".mage_health_points").innerHTML = (characters.Mage.health_points);
 	document.querySelector(".hunter_health_points").innerHTML = (characters.Hunter.health_points);
@@ -49,9 +43,8 @@ $(document).ready(function() {
 
 
 	$(".character-boxes").on("click", function() {
-		if (is_character_chosen == false){				//if you haven't chosen a char yet
+		if ($(".character-chosen").children().length == 1){				//if you haven't chosen a char yet
 			$(".character-chosen").append(this);		//adds char chosen/clicked on to character chosen div
-			is_character_chosen = true;					//means you've now chosen a character, can't choose more
 			character_chosen = this;					//iniates character chosen to whatever clicked on.
 			$(".alert-text").html("Choose an enemy!");
 		}
@@ -59,22 +52,22 @@ $(document).ready(function() {
 			if (this !== character_chosen){				//making sure that it's not the one you chose to fight	
 				$(".enemies-available").append(this);	//and otherwise adds to "enemies available" list
 			}											//closing for if
-			choose_enemy(is_enemy_chosen);
+			choose_enemy();
 		});												//closing for .each func												
 	});													//closing for on click
 
 	
 	$("button").on("click", function(){
-		if (($(".enemy-chosen").children().length > 1 )&& ($(".character-chosen").children().length > 1 )){
+		if (($(".enemy-chosen").children().length > 1 )&& ($(".character-chosen").children().length > 1 )){ //only lets you enter this if you have a char chosen and an enemy chosen
 			var attacker;
-
+			//checks to see what class the character box under character chosen has
 			if ($(".character-chosen").children().hasClass("rogue")){				attacker = characters.Rogue;}
 			else if ($(".character-chosen").children().hasClass("death-knight")){	attacker = characters.DeathKnight;}
 			else if ($(".character-chosen").children().hasClass("mage")){			attacker = characters.Mage;}
-			else {																attacker = characters.Hunter;}
+			else {																	attacker = characters.Hunter;}
 
 
-			var defender; 
+			var defender; 				//checks to see what class the character box under enemy chosen has
 			if ($(".enemy-chosen").children().hasClass("rogue")){				
 				defender = characters.Rogue;
 			}
@@ -88,7 +81,7 @@ $(document).ready(function() {
 				defender = characters.Hunter;
 			}
 
-			if ((defender.health_points < 0) || ((defender.health_points -= attacker.attack_power) < 0)){
+			if ((defender.health_points < 0) || ((defender.health_points -= attacker.attack_power) < 0)){	//enters if enemy is dead or if your next attack will kill him
 				$(".alert-text").html("Bro. You already won. C'mon, why u still attack?");
 				$(".enemy-chosen .character-boxes").remove();	//deletes the character when they deaddddd
 				if ($(".enemies-available").children().length < 2){		//creates "you won!!!" text when there are no more enemies available
